ce334db01a822a33e490adfac6e87e7e9f334580
Gmailvicente04 <vicente.oswald@gmail.com>
2025-03-15 14:05:13 -0400

    "Task 1 - Base Structure Setup."

---
+++ cc_15.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Risk Dashboard Loaded");
+
+    // Task 1: Select the riskDashboard container
+    const riskDashboard = document.getElementById("riskDashboard");
+    const riskForm = document.getElementById("riskForm");
+    const increaseRiskBtn = document.getElementById("increaseRiskLevels");
+
+} );
+
+
